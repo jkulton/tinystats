7ea58586f947625fd52f63dcb434740077408dab
jkulton <jonkulton@gmail.com>
2020-03-11 23:30:52 -0400

    Clean up main server file

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ fastify.get('/', function (req, reply) {
   reply.sendFile('index.html');
 });
 
-// fastify.get('/path/with/different/root', function (req, reply) {
-//   reply.sendFile('myHtml.html', path.join(__dirname, 'build')) // serving a file from a different root location
-// })
-
 fastify.listen(3000, err => {
   if (err) throw err
 });
